e9caf9fcd1396acf16fcff76952edc26e98c8335
hector23rp <hector23rp@gmail.com>
2019-03-18 20:33:53 +0100

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'TECLI1819-Explorer-CodeJOH';
+}
