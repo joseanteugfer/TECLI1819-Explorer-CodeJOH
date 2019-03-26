3886097191d49d155fe0c4937b6d1a80fdafc19d
hector23rp <hector23rp@gmail.com>
2019-03-26 20:30:48 +0100

    Login (#10)
    * Added login
    
    * Material desgin
    
    * Material Design
    
    * added basic footer
    
    * Deleted folder header

--- src/app/components/master/footer/footer.component.ts
+++ src/app/components/master/footer/footer.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  today: number;
   constructor() { }
 
   ngOnInit() {
+    this.today = Date.now();
   }
 
 }
